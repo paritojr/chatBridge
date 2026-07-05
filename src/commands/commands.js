cbffeeb231eaac9e1a2ca444b7abf8a9a6c4cec5
IAmGaryTheCat <95551311+IAmGaryTheCat@users.noreply.github.com>
2026-07-05 11:41:17 -0500

    help command and move old help command

---
+++ src/commands/commands.js
@@ -0,0 +1,19 @@
+import { client } from "../client.js";
+import { PREFIX } from "../utils.js";
+
+
+
+export default {
+    name: "commands",
+    description: "list all commands",
+    usage: "commands",
+    async execute(message) {
+        const commands = client.textCommands.map(command => {
+            return `**${command.name}**: ${command.description} \nUsage: ${PREFIX}${command.usage}`;
+        }).join("\n\n");
+
+        message.reply(commands);
+
+
+    },
+};
--- src/commands/help.js
+++ src/commands/help.js
@@ -1,18 +1,23 @@
 import { client } from "../client.js";
+import { PREFIX } from "../utils.js";
 
 
 
 export default {
     name: "help",
-    description: "list all commands",
+    description: "get instructions for how to use the bot",
     usage: "help",
     async execute(message) {
-        const commands = client.textCommands.map(command => {
-            return `**${command.name}**: ${command.description} \nUsage: ${command.usage}`;
-        }).join("\n\n");
 
-        message.reply(commands);
-
+        const messageContent =
+            `
+        ChatBridge Instructions:\n
+        1. Add the bot to your Discord server.\n
+        2. Create a channel in your Discord server that you want to link to the chat bridge.\n
+        3. Use the command \`${PREFIX}link <channel>\`  to link it to the chat bridge.\n
+        4. To unlink the channel, use the command \`${PREFIX}unlink\`.  
+        `
+        message.reply(messageContent);
 
     },
 };
